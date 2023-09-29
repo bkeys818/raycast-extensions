0c5aa14c71804bce66e4e9e42d49979b0de14044
Ben Keys <bkeys818@gmail.com>
2023-09-29 10:45:55 -0400

    🚚 Sort notion api calls
    This commit moves around a lot of code, but there is no change in functionality.

---
+++ extensions/notion/src/utils/notion/user.ts
@@ -0,0 +1,24 @@
+import { authorize, notion } from "./authorize";
+import { handleError, isNotNullOrUndefined } from "./global";
+import { User } from "../types";
+
+export async function fetchUsers() {
+  try {
+    await authorize();
+    const users = await notion.users.list({});
+    return users.results
+      .map((x) => (x.object === "user" && x.type === "person" ? x : undefined))
+      .filter(isNotNullOrUndefined)
+      .map(
+        (x) =>
+          ({
+            id: x.id,
+            name: x.name,
+            type: x.type,
+            avatar_url: x.avatar_url,
+          }) as User,
+      );
+  } catch (err) {
+    return handleError(err, "Failed to fetch users", []);
+  }
+}
