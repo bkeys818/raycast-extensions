218a5ff6a0428639be345e5396b22f20a8b29a4b
Ben Keys <bkeys818@gmail.com>
2024-01-18 11:38:36 -0500

    🚧 Create methods for remaining tag endpoints

--- extensions/toggl-track/src/api/tags.ts
+++ extensions/toggl-track/src/api/tags.ts
@@ -1,7 +1,19 @@
-import { get } from "./togglClient";
+import { get, post, put, remove } from "./togglClient";
 
 export function getTags(workspaceId: number) {
   return get<Tag[] | null>(`/workspaces/${workspaceId}/tags`);
+}
+
+export function createTag(workspaceId: number, name: string) {
+  return post<Tag[] | null>(`/workspaces/${workspaceId}/tags`, { workspace_id: workspaceId, name });
+}
+
+export function updateTag(tag: Tag, name: string) {
+  return put<Tag[] | null>(`/workspaces/${tag.workspace_id}/tags/${tag.id}`, { workspace_id: tag.workspace_id, name });
+}
+
+export function deleteTag(tag: Tag) {
+  return remove(`/workspaces/${tag.workspace_id}/tags/${tag.id}`);
 }
 
 // https://developers.track.toggl.com/docs/api/tags#response
