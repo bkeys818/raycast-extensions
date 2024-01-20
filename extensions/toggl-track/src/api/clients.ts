c99027d24dfd5ecc0adc2bd20bdef75e6807a2ad
Ben Keys <bkeys818@gmail.com>
2024-01-20 16:18:18 -0500

    🚧 Create methods for remaining client endpoints

--- extensions/toggl-track/src/api/clients.ts
+++ extensions/toggl-track/src/api/clients.ts
@@ -1,7 +1,19 @@
-import { get } from "./togglClient";
+import { get, post, put, remove } from "./togglClient";
 
 export function getClients(workspaceId: number) {
   return get<Client[] | null>(`/workspaces/${workspaceId}/clients`);
+}
+
+export function createClient(workspaceId: number, name: string) {
+  return post<Client>(`/workspaces/${workspaceId}/clients`, { name, wid: workspaceId });
+}
+
+export function updateClient(workspaceId: number, clientId: number, name: string) {
+  return put<Client>(`/workspaces/${workspaceId}/clients/${clientId}`, { name, wid: workspaceId });
+}
+
+export function deleteClient(workspaceId: number, clientId: number) {
+  return remove(`/workspaces/${workspaceId}/clients/${clientId}`);
 }
 
 // https://developers.track.toggl.com/docs/api/clients#response
--- extensions/toggl-track/src/api/index.ts
+++ extensions/toggl-track/src/api/index.ts
@@ -1,7 +1,7 @@
 export { getMe, type Me } from "./me";
 export { getWorkspaces, type Workspace } from "./workspaces";
 export { getProjects, type Project } from "./projects";
-export { getClients, type Client } from "./clients";
+export { getClients, createClient, updateClient, deleteClient, type Client } from "./clients";
 export { getTags, type Tag } from "./tags";
 export { getTasks, type Task } from "./tasks";
 export { getTimeEntries, createTimeEntry, stopTimeEntry, getRunningTimeEntry, type TimeEntry } from "./timeEntries";
